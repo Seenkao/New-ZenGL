c8cd244942fc6395dc553fd199922ceed70bc07d
Seenkao <62959889+Seenkao@users.noreply.github.com>
2021-07-21 23:13:34 +0300

    Update macosx_postbuild.sh

--- Zengl_SRC/demos/Lazarus/17 - Menu(touch menu)/macosx_postbuild.sh
+++ Zengl_SRC/demos/Lazarus/17 - Menu(touch menu)/macosx_postbuild.sh
@@ -2,6 +2,7 @@
 cp ../../../bin/data/font* ../../../bin/demo17.app/Contents/Resources/
 cp ../../../bin/data/CalibriBold50pt* ../../../bin/demo17.app/Contents/Resources/
 cp ../../../bin/data/arrow.png ../../../bin/demo17.app/Contents/Resources/
+cp ../../../bin/data/Rus.txt ../../../bin/demo17.app/Contents/Resources/
 # copy binary into bundle
 rm ../../../bin/demo17.app/Contents/MacOS/demo17
 cp ../../../bin/demo17 ../../../bin/demo17.app/Contents/MacOS/demo17
