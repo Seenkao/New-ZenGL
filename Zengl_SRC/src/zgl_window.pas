db422375dc4db80bc01e39c8ada5466c351b3152
Seenkao <62959889+Seenkao@users.noreply.github.com>
2021-07-26 16:56:30 +0300

    Update zgl_window.pas

--- Zengl_SRC/src/zgl_window.pas
+++ Zengl_SRC/src/zgl_window.pas
@@ -934,8 +934,6 @@ end;
 
 {$IfNDef USE_INIT_HANDLE}
 procedure zgl_Init(FSAA: Byte = 0; StencilBits: Byte = 0);
-var
-  i: word; 
 begin
   {$IfNDef ANDROID}
   zgl_GetSysDir();
@@ -983,8 +981,6 @@ begin
     wndCaption := cs_ZenGL;
   wnd_UpdateCaption();
   {$EndIf}
-  for i := 0 to 255 do
-    keysUp[i] := true; 
   winOn := TRUE;
 
   {$IFDEF iOS}
